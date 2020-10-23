4af8128ab45695315d17948293c5c21117ad6c05
Sara Morillon <sara.morillon@gmail.com>
2020-10-23 13:00:30 +0200

    Whiter white

--- src/colors.ts
+++ src/colors.ts
@@ -2,7 +2,7 @@ import { Property } from 'csstype'
 
 export const colors: { [key: string]: Property.Color } = {
   main: 'crimson',
-  white: '#f2f3f5',
+  white: '#fcfdff',
   black: '#303030',
   lightgrey: '#e8e9eb',
   grey: '#b2b3b4',
